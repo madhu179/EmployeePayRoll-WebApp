c27ece31822eae55e03fc8e178c49a2a18deba14
madhu179 <madhubabu232343@gmail.com>
2020-11-17 16:36:57 +0530

    Adding code to view Employee details from json object in table format

--- EmployeePayRoll-JsonServer/js/home.js
+++ EmployeePayRoll-JsonServer/js/home.js
@@ -12,24 +12,65 @@ const createInnerHtml = () => {
     <th>Start Date</th>
     <th>Actions</th>
   `;
-  const innerHtml = `${headerHtml}
+  let innerHtml = `${headerHtml}`;
+  let empPayrollList = createEmployeePayrollJSON();
+  for(const empPayrollData of empPayrollList)
+  {
+  innerHtml = `${innerHtml}
   <tr>
       <td>
-      <img class="profile" alt="" src="../assets/profile-images/Ellipse -3.png">
+      <img class="profile" alt="" src="${empPayrollData._profilePic}">
       </td>
-      <td>Anthony Stark</td>
-      <td>Male</td>
-      <td><div class="dept-label">Finance</div>
-          <div class="dept-label">Engineer</div></td>
-      <td>499999</td>
-      <td>1 Jan 2016</td>
+      <td>${empPayrollData._name}</td>
+      <td>${empPayrollData._gender}</td>
+      <td><div class="dept-label">${getDeptHtml(empPayrollData._department)}</div></td>
+      <td>${empPayrollData._salary}</td>
+      <td>${empPayrollData._startDate}</td>
       <td>
-      <img id="1" onclick="removie(this)" alt="delete" 
+      <img name="${empPayrollData._id}" onclick="removie(this)" alt="delete" 
               src="../assets/icons/delete-black-18dp.svg">
-      <img id="1" alt="edit" onclick="update(this)"
+      <img name="${empPayrollData._id}" alt="edit" onclick="update(this)"
               src="../assets/icons/create-black-18dp.svg">
       </td>
   </tr>
   `;
+  }
 document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+const getDeptHtml = (deptList) => {
+let deptHtml = '';
+for(const dept of deptList){
+  deptHtml = `${deptHtml} <div class="dept-label">${dept}</div>`
+}
+return deptHtml;
+}
+
+const createEmployeePayrollJSON = () => {
+  let empPayrollListLocal = [
+  {
+    _name: 'Anthony Stark',
+    _gender: 'male',
+    _department: [
+      'Finance',
+      'Engineer'
+    ],
+    _salary: '499999',
+    _startDate: '14 May 2016',
+    _note: 'Hi There',
+    _id: new Date().getTime(),
+    _profilePic: '../assets/profile-images/Ellipse -3.png'
+  },
+  {
+    _name: 'Natasha',
+    _gender: 'female',
+    _department: ['Others'],
+    _salary: '299999',
+    _startDate: '21 Feb 2020',
+    _note: 'Hello',
+    _id: new Date().getTime(),
+    _profilePic: '../assets/profile-images/Ellipse -1.png'
+  }
+  ];
+  return empPayrollListLocal;
 }
